35bfc1e8e63b185860debc58ba4f86baaffd3a5e
Marie Burns <mcburns2222@gmail.com>
2018-11-21 00:23:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,5 +38,5 @@ def sum_array(integers)
 end
   
 def add_s(words)
-  words.each_with_index.collect {|element, index| element << "s" if index != 1}
+  words.each_with_index.collect {|element, index| element << "s" if index != 1 else element}
 end
