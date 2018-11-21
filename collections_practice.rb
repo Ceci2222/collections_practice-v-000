f15db7d67c469af2f3b9dac1e386fb811a80c352
Marie Burns <mcburns2222@gmail.com>
2018-11-21 00:06:50 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  sorted_integers = integers.sort
+  sorted_integers.reverse
+end
+
+def sort_array_char_count(words)
+  words.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+  second = array[1]
+  array[1] = array[2]
+  array[2] = second
+  array
+end
+
+def reverse_array(integers)
+  integers.reverse 
+end
+
+def kesher_maker(strings)
+  
